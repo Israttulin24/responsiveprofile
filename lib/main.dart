1fbc9b126f2d2f43b6344cf49dfa2dbc9a1fc212
Israt Jahan Tulin <135654667+Israttulin24@users.noreply.github.com>
2023-07-19 03:51:34 +0600

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -26,16 +26,19 @@ class _MyHomePageState extends State<HomeResponsive> {
   @override
   Widget build(BuildContext context) {
     return OrientationBuilder(
-      builder: (context, orientation){
-        final isPortrait = orientation == Orientation.portrait;
+        builder: (context, orientation){
+          final isPortrait = orientation == Orientation.portrait;
 
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text('Profile'),
-          ),
-          body: Column (
-            children: [
-              Padding(padding: EdgeInsets.all(10)),
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Profile'),
+            ),
+
+            body: MediaQuery.of(context).orientation == Orientation.portrait
+            ?SingleChildScrollView(
+              child: Column (
+              children: [
+                Padding(padding: EdgeInsets.all(10)),
                 Center (
                   child: Column(
                     children: [
@@ -50,60 +53,92 @@ class _MyHomePageState extends State<HomeResponsive> {
                         padding: EdgeInsets.only(left: 7,right: 3),
                         child: Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",style: TextStyle(fontSize: 15),
                         ),
-                      )
+                      ),
+                      Padding(padding: EdgeInsets.only(top: 20)),
+
+                      Wrap(
+                        runSpacing: 8.0,
+                        spacing: 8.0,
+                        children: [
+                          Image.asset('images/cat.jpg',fit: BoxFit.fill,width: 130,height: 130,),
+                          Image.asset('images/cat.jpg',fit: BoxFit.fill,width: 130,height: 130,),
+                          Image.asset('images/cat.jpg',fit: BoxFit.fill,width: 130,height: 130,),
+                          Image.asset('images/cat.jpg',fit: BoxFit.fill,width: 130,height: 130,),
+                          Image.asset('images/cat.jpg',fit: BoxFit.fill,width: 130,height: 130,),
+                          Image.asset('images/cat.jpg',fit: BoxFit.fill,width: 130,height: 130,),
+
+
+                        ],
+                      ),
+
                     ],
+                  ),
                 ),
-                ),
+              ],
+            ),
 
-              Card(
-                child: Text("hi"),
+          )
+
+            :
+
+            SingleChildScrollView(
+              child: Row (
+                children: [
+                  Padding(padding: EdgeInsets.all(5)),
+                  Row(
+                      children: [
+                        Center(
+                          child: CircleAvatar(
+                            radius: 140,
+                            backgroundImage:AssetImage('images/cat.jpg'),
+                          ),
+                        ),
+                        Padding(padding: EdgeInsets.only(right: 20)),
+                      ],
+                    ),
+                  Container(
+                    width: 400,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(padding: EdgeInsets.only(top: 10)),
+                        Text("John Doe",style: TextStyle(fontSize: 23,fontWeight: FontWeight.bold),),
+                        Padding(padding: EdgeInsets.only(top: 10)),
+                        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",style: TextStyle(fontSize: 15),),
+                        Padding(padding: EdgeInsets.only(top: 10)),
+
+                        Wrap(
+                          runSpacing: 8.0,
+                          spacing: 8.0,
+                          children: [
+                            Image.asset('images/cat.jpg',fit: BoxFit.fill,width: 100,height: 100,),
+                            Image.asset('images/cat.jpg',fit: BoxFit.fill,width: 100,height: 100,),
+                            Image.asset('images/cat.jpg',fit: BoxFit.fill,width: 100,height: 100,),
+                            Image.asset('images/cat.jpg',fit: BoxFit.fill,width: 100,height: 100,),
+                            Image.asset('images/cat.jpg',fit: BoxFit.fill,width: 100,height: 100,),
+                            Image.asset('images/cat.jpg',fit: BoxFit.fill,width: 100,height: 100,),
+
+
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+
+
+
+                ],
               ),
-              GridView.builder(
-                itemCount: 4,
-                itemBuilder: (context,index){
-                  return Card(
-                    elevation: 10,
-                    child: Image.asset('images/cat.jpg'),
-                  );
 
-                }, gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-                // children: List.generate(
-                //     6, (index) => Card(
-                //   color: Colors.orange,
-                //   //child:Image.asset('images/cat.jpg'),
-                // ),
-                // ),
-              ),
-            ],
-          ),
-
-
-
-          // Container(
-          //   child: GridView.count(
-          //       crossAxisCount: isPortrait ? 2:3,
-          //       children: List.generate(
-          //           6, (index) => Card(
-          //             color: Colors.orange,
-          //             child:Image.network('https://play-lh.googleusercontent.com/fWOFhAaXbqYILb7AzCelO2d39HFZtH17suMMsdHPP_6RgfI-jDdx14Sg1fl7D1AaBHVX=w2560-h1440-rw',fit: BoxFit.fill,width: 120,height: 120,),
-          //
-          //
-          //
-          //           )
-          //
-          //       )
-          //
-          //
-          //   ),
-          // )
+            )
 
 
 
 
-        );
-        
-        
-      }
+          );
+
+
+        }
 
       // appBar: AppBar(
       //   title: const Text('Profile'),
@@ -118,6 +153,7 @@ class _MyHomePageState extends State<HomeResponsive> {
       //   color: Colors.green,
       //   child: const Center(child: Text("Landscape"),),
       // )
+
     );
   }
 }
